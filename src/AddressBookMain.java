a2d4a2e23205b8b00a7d7b9c895d20d440a8f0c1
mukesh007kls <mallikanti2000@gmail.com>
2023-03-04 14:24:14 +0530

    ADDED:Code to edit contact details

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class AddressBookMain {
     public static void main(String[] args) {
         final int ADD_CONTACT = 1;
-        final int PRINT_CONTACT = 2;
-        final int EXIT=3;
+        final int EDIT_CONTACT=2;
+        final int PRINT_CONTACT = 3;
+        final int EXIT = 4;
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Address Book");
         AddressBookOperations addressBookOperations = new AddressBookOperations();
         boolean loop = true;
         while (loop) {
-            System.out.println("Enter the choice:-\n1.Add a new contact\n2.Display the Contacts\n3.To exit");
+            System.out.println("Enter the choice:-\n1.Add a new contact\n2.Edit Contact Details\n3.Display the Contacts\n4.To exit");
             int choice = sc.nextInt();
             switch (choice) {
                 case ADD_CONTACT -> addressBookOperations.readDetails();
+                case EDIT_CONTACT -> addressBookOperations.editDetails();
                 case PRINT_CONTACT -> addressBookOperations.printDetails();
-                case EXIT->loop=false;
+                case EXIT -> loop = false;
                 default -> throw new IllegalStateException("Unexpected value: " + choice);
             }
         }
--- src/AddressBookOperations.java
+++ src/AddressBookOperations.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class AddressBookOperations {
     final List<Address> addressList = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
 
     public void readDetails() {
         boolean addContact = true;
@@ -34,7 +35,7 @@ public class AddressBookOperations {
             System.out.println("Enter State:");
             address1.setState(sc1.nextLine());
 
-            System.out.println("Enter Pincode:");
+            System.out.println("Enter PinCode:");
             address1.setPinCode(sc1.nextInt());
 
             Address address2 = new Address(address1.getFirstName(), address1.getLastName(), address1.getPhoneNumber(),
@@ -42,13 +43,87 @@ public class AddressBookOperations {
                     address1.getPinCode());
             addressList.add(address2);
             System.out.println("If you want add another contact enter add or else enter exit:-");
-            String choice = sc1.next();
-            if (choice.equalsIgnoreCase("add")) {
-                addContact = true;
-            } else {
-                addContact = false;
-            }
+            String choice = sc.next();
+            addContact = choice.equalsIgnoreCase("add");
         }
+    }
+
+    public void editDetails() {
+        final int FIRST_NAME = 1;
+        final int LAST_NAME = 2;
+        final int PHONE_NUMBER = 3;
+        final int EMAIL = 4;
+        final int ADDRESS = 5;
+        final int CITY = 6;
+        final int STATE = 7;
+        final int PIN_CODE = 8;
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Enter First Name:-");
+        String firstName = sc2.nextLine();
+        System.out.println("Enter Last Name:-");
+        String lastName = sc2.nextLine();
+        boolean loop = true;
+        while (loop) {
+            for (Address x : addressList) {
+                if (x.getFirstName().equalsIgnoreCase(firstName) && x.getLastName().equalsIgnoreCase(lastName)) {
+                    System.out.println("Choose which detail you want to change:-");
+                    System.out.println("1.First name\n2.Last Name\n3.Phone Number\n4.EMail\n5.Address\n6.City\n7.State\n8.PinCode");
+                    int choice = sc2.nextInt();
+                    sc2.nextLine();
+                    switch (choice) {
+                        case FIRST_NAME -> {
+                            System.out.println("Enter the New First Name:-");
+                            x.setFirstName(sc2.nextLine());
+                        }
+                        case LAST_NAME -> {
+                            System.out.println("Enter the New Last Name:-");
+                            x.setLastName(sc2.nextLine());
+                        }
+                        case PHONE_NUMBER -> {
+                            System.out.println("Enter the New Phone Number:-");
+                            x.setPhoneNumber(sc2.nextLong());
+                        }
+                        case EMAIL -> {
+                            System.out.println("Enter the New Email ID:-");
+                            x.seteMail(sc2.nextLine());
+                        }
+                        case ADDRESS -> {
+                            System.out.println("Enter the New Address:-");
+                            x.setAddress(sc2.nextLine());
+                        }
+                        case CITY -> {
+                            System.out.println("Enter the New City:-");
+                            x.setCity(sc2.nextLine());
+                        }
+                        case STATE -> {
+                            System.out.println("Enter the new state:-");
+                            x.setState(sc2.nextLine());
+                        }
+                        case PIN_CODE -> {
+                            System.out.println("Enter New PinCode");
+                            x.setPinCode(sc2.nextInt());
+                        }
+                    }
+                    updatedContactDetail(x);
+                }
+                firstName = x.getFirstName();
+                lastName = x.getLastName();
+            }
+            System.out.println("If you want to change any other details of this contact enter edit:-");
+            String editContact = sc.next();
+            loop = editContact.equalsIgnoreCase("edit");
+        }
+    }
+
+    public void updatedContactDetail(Address address) {
+        System.out.println("Updated Details of contact");
+        System.out.println("Name:-" + address.getFirstName() + " " + address.getLastName());
+        System.out.println("Phone Number:-" + address.getPhoneNumber());
+        System.out.println("EMail:-" + address.geteMail());
+        System.out.println("Address:-" + address.getAddress());
+        System.out.println("City:-" + address.getCity());
+        System.out.println("State:-" + address.getState());
+        System.out.println("PinCode:-" + address.getPinCode());
     }
 
     public void printDetails() {
@@ -61,7 +136,7 @@ public class AddressBookOperations {
             System.out.println("City:-" + x.getCity());
             System.out.println("State:-" + x.getState());
             System.out.println("PinCode:-" + x.getPinCode());
-            System.out.println("");
+            System.out.println();
         }
     }
 }
